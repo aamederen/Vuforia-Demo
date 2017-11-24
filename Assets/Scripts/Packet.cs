18d0d2e1a2626ca3da07cd9fb796ed9106b6934a
Eren Başak <aamederen@gmail.com>
2017-11-24 11:56:41 +0300

    Show animation when an elicorn is shown

---
+++ Assets/Scripts/Packet.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Packet : MonoBehaviour {
+
+	public const float THRESHOLD = 0.001f;
+
+	public float Speed = 0.2f;
+	
+	private List<Vector3> path = new List<Vector3>();
+	
+	public void SetPath(Vector3[] points) {
+		path.Clear();
+		foreach (var point in points) {
+			path.Add(point);
+		}
+	}
+
+	void Update () {
+		Debug.Log("path.count = " + path.Count);
+		// do nothing if there are no points in the path for me to follow
+		if (path.Count == 0) {
+			Destroy(this.gameObject);
+			return;
+		}
+
+		var nextPoint = path[0];
+
+		if (Vector3.Distance(transform.position, nextPoint) < THRESHOLD) {
+			Debug.Log("Checkpoint! path.count = " + path.Count);
+			path.RemoveAt(0);
+			nextPoint = path.Count == 0 ? new Vector3(0,0,0) : path[0];
+		}
+
+		if(nextPoint != null) {
+			transform.position = Vector3.MoveTowards(transform.position, 
+																							 nextPoint,
+																							 Speed * Time.deltaTime);
+		}
+	}
+}
---
+++ Assets/Scripts/PacketSpawner.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PacketSpawner : MonoBehaviour {
+
+	public Transform PacketObject = null;
+	public float TimeToCreatePacket = 0.5f;
+	public float PacketSpeedMin = 1.0f;
+	public float PacketSpeedMax = 1.5f;
+
+	private GameObject PacketsContainer = null;
+	private List<Vector3[]> Paths = new List<Vector3[]>();
+
+	private float LastPacketCreatedAt = 0.0f;
+	private int NextPath = 0;
+
+	// Use this for initialization
+	void Start () {
+		PacketsContainer = GameObject.Find("Packets");
+		
+		var linesContainer = GameObject.Find("Lines");
+		var lineRendererComponents = linesContainer.GetComponentsInChildren<LineRenderer>();
+
+		foreach (LineRenderer lineRenderer in lineRendererComponents) {
+			int pointCount = lineRenderer.positionCount;
+			Vector3[] points = new Vector3[pointCount];
+			lineRenderer.GetPositions(points);
+			for(int i = 0; i < points.Length; i++)
+			{
+				Debug.Log("Point before: " + points[i]);
+				points[i] = lineRenderer.transform.TransformPoint(points[i]);
+				Debug.Log("Point after: " + points[i]);
+			}
+			Paths.Add(points);
+		}
+
+		LastPacketCreatedAt = Time.time;
+
+		Debug.Log("Initialized PacketSpawner");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (PacketObject == null)
+		{
+			return;
+		}
+
+		float now = Time.time;
+		if (now > LastPacketCreatedAt + TimeToCreatePacket)
+		{
+			var path = Paths[NextPath];
+
+			var packetObject = Instantiate(PacketObject, path[0], Quaternion.identity);
+			var packet = packetObject.GetComponent<Packet>();
+
+			packetObject.transform.parent = PacketsContainer.transform;
+			packet.SetPath(path);
+			packet.Speed = Random.Range(PacketSpeedMin, PacketSpeedMax);
+			NextPath = (NextPath + 1) % Paths.Count;
+			LastPacketCreatedAt = now;	
+		}
+	}
+}
